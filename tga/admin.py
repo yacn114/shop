f8460e437c4acb05ed9b8e7ec0859184b5d66be2
salar <127493852+swlrx08@users.noreply.github.com>
2024-07-06 00:29:02 -0700

    sql

--- tga/admin.py
+++ tga/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product,Category,returned
+from .models import Product,Category,returned,Stars
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(returned)
+admin.site.register(Stars)
---
+++ tga/templatetags/star.py
@@ -0,0 +1,7 @@
+# from django import template
+# from tga.models import Stars
+# register = template.Library()
+
+# @register.simple_tag
+# def star():
+#     pass
